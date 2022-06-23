8655b4923d39d4e8009708e2133e2978cbe52360
toyolife881 <toyolife.island881@gmail.com>
2022-06-23 17:44:55 +0900

    ログアウト機能の実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,6 +12,11 @@ class UsersController < ApplicationController
     end
   end
 
+  def destroy
+  end
+
+
+
 
   private
   def user_params
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "messages#index"
 
-  resources :users, only: [:edit, :update]
+  resources :users, only: [:edit, :update, :destroy]
 end
