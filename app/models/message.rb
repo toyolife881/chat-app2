a5708870d05632c63f666acc587cbe6b9ba8294e
toyolife881 <toyolife.island881@gmail.com>
2022-06-25 21:31:57 +0900

    画像のみでもメッセージ送信可能となるバリデーションの設定

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,8 +1,12 @@
 class Message < ApplicationRecord
 
-  validates :content , presence: true
+  validates :content , presence: true, unless: :was_attached?
 
   belongs_to :user
   belongs_to :room
   has_one_attached :image
+
+  def was_attached?
+    self.image.attached?
+  end
 end
